43e0b8ddcbcf102722ce4fa3f1ea90facd178d5e
chrismadufor <chrismadufor.com>
2022-08-07 11:57:38 +0100

    font fix

--- src/pages/Scoresheet.jsx
+++ src/pages/Scoresheet.jsx
@@ -9,7 +9,7 @@ function Scoresheet({ type }) {
   const pageData = data[type];
   return (
     <div className="bg-black text-white">
-      <header className="flex items-center justify-between h-28 px-10 bg-gray-100 text-black">
+      <header className="flex items-center justify-between h-24 px-5 bg-gray-100 text-black">
         <div className="flex items-center">
         <i onClick={() => navigate('/')} className="fa-solid fa-home mr-10 text-4xl cursor-pointer"></i>
         <p className="text-5xl uppercase font-bold">
@@ -18,7 +18,7 @@ function Scoresheet({ type }) {
         </div>
         <Timer />
       </header>
-      <div className="grid grid-cols-2 gap-5 gap-x-8 p-10 pt-16">
+      <div className="grid grid-cols-2 gap-5 gap-x-8 py-10 px-5 pt-16">
         {pageData.participants.map((participant, index) => (
           <UserCard key={index} number={index} name={participant} />
         ))}
