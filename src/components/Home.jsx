c8b65cbd2dc07768fcb1fb26861734864fee9c1c
codes001 <prynzwill147@gmail.com>
2024-07-27 18:08:46 +0100

    first commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,7 +5,7 @@ function Home() {
 const navigate = useNavigate()
   return (
     <div className='home text-white flex flex-col items-center justify-center h-screen'>
-        <h1 className='text-5xl font-bold mb-10'>Welcome To The Nyanya Youth Week, 2023</h1>
+        <h1 className='text-5xl font-bold mb-10'>Welcome To The Nyanya Youth Quiz, 2024</h1>
         <h1 className='text-5xl'>Quiz Competition (1st Round)</h1>
         <div className='flex items-center mt-10'>
         <button className='bg-white text-black px-8 py-3 block text-3xl font-bold rounded-md mx-2' onClick={() => navigate('first')}>Ages 6-10</button>
