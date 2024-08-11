a837f98b1374a5e3cf0dd9c6be36b078ca085a3c
codes001 <prynzwill147@gmail.com>
2024-08-11 11:52:40 +0100

    Modified names

--- src/data.js
+++ src/data.js
@@ -2,21 +2,13 @@ const data = {
   first: {
     title: "Ages 6-10",
     participants: [
-      "Collins",
-      "Mfonobong Emaeyak",
+     
       "Dominion Friday Edet",
-      "Goodheart Jackson",
       "Imaobong Itoro Edu",
-      "Godgift Ajunwa",
       "Splendor Chidiebube Nwabueze",
       "Oboba Great",
-      "Ajunwa Victor Chiziruzo",
       "Attah George Emmanuel",
       "Attah George Queen Elizabeth",
-      "Adejo Ajogwu Oloche",
-      "Mfonobong Itone Edu",
-      "Semediong Uduak Friday",
-      "Confidence Moses Icha",
       "Purity Moses Icha",
       "Mmeyene Uduak Friday"
     ],
@@ -24,17 +16,12 @@ const data = {
   second: {
     title: "Ages 11-15",
         participants: [
-      "Oche Yusuf Bobby",
       "Ben Nnamma",
-      "Joel Sydney Asuquo",
       "Esiet Etinyene Donald",
-      "Collins Yusuf Bobby",
       "Medara Clement Essien",
       "George Itoro Edu",
-      "Edu Etienoabasi Itora",
+      "Edu Etienoabasi Itoro",
       "Ohiri Favour Chioma",
-      "Enyo-Ojo Samson Goodness",
-      "Edith Moses Icha",
       "Ochanya Yusuf Bobby",
       "Gideon Friday Edet",
     ],
